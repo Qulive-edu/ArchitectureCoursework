6e46cfd30d021f70c61481411993d0f5fda357cf
Qulive-edu <assasinplay306@gmail.com>
2026-05-16 22:48:33 +0300

    seed change

--- scripts/seed.sql
+++ scripts/seed.sql
@@ -1,9 +1,8 @@
--- Используйте price_per_hour вместо price
 INSERT INTO places (title, address, floor_type, description, image, "price_per_hour")
 VALUES
-('Спортзал №1', 'ул. Ленина, 10', 'Большой спортивный зал для игр', 'https://volleygrad.ru/wp-content/uploads/2021/03/photo_2022-11-09_21-14-33.jpg', 1500),
-('Футбольное поле', 'пр. Победы, 22', 'Открытое футбольное поле', 'https://findsport.ru/userfiles/images/620x365/1654861015_b97bdb3f0aad94547f4bdbd9d6ef73c4.jpg', 2500),
-('Баскетбольная площадка', 'ул. Молодёжная, 7', 'Закрытая площадка для баскетбола', 'https://terball.ru/sites/default/files/styles/crop_1000_800/public/2020-05/basketball-rent-gallery-6.jpg?itok=5LPm2h2C', 2000);
+('Парковка №1', 'ул. Ленина, 10', 'Большая парковка', 'https://avtoshkola.msk.ru/wp-content/uploads/2022/03/parkovka-elochkoi.jpg', 1500),
+('Парковка №2', 'пр. Победы, 22', 'Открытая парковка', 'https://blog.idn500.ru/upload/iblock/3b3/ercbd0j26hu5aspiiy159xx09j1n4z20/ploskostnye_parkovki.jpg', 2500),
+('Парковка №3', 'ул. Молодёжная, 7', 'Закрытая парковка', 'https://rosdorznakservis.ru/files/uploads/statii/parkovka-i-avtostoyanka-razbiraemsya-gde-ostavit-mashinu/parkovka.jpg', 2000);
 
 INSERT INTO time_slots (place_id, start_time, end_time, is_available)
 VALUES
